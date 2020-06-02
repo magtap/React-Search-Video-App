4fda751ed9363986abaa7c17d87d051e1f762e5c
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2020-06-02 14:25:59 +0700

    create VideoItem

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
+const VideoList = ({ videos }) => {
+    const renderedList = videos.map( video => {
+        return <VideoItem />;    
+    });
+
+
     return (
-        <div>{props.videos.length}</div>
+        <div>{renderedList}</div>
     );
 };
 
